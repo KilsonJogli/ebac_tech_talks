03a97c4f94cf59f8f73fcc08efed2b99fd15291b
Kilson Alves <killwines@gmail.com>
2023-09-05 16:18:44 -0300

    criando a contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,32 @@
-console.log('olá')
+AOS.init();
+
+const dataDoEvento = new Date("Dec 25, 2023 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+    
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % (1000 * 60 * 60)) / (1000 * 60));
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % (1000 * 60)) / 1000);
+
+    document.getElementById('contator').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`
+
+console.log(horasAteOEvento)
+console.log(minutosAteOEvento)
+
+if(distanciaAteOEvento < 0) {
+    clearInterval(contaAsHoras);
+    document.getElementById('contator').innerHTML = 'Evento expirado'
+}
+
+},1000)
